bb4531a81f7ac763818504eade71c7ef47e4b446
KimYeonWook511 <kimyeonwook511@gmail.com>
2025-12-09 17:19:52 +0900

    feat(concurrency-section6): inner class로 수정 - 다른 학습 내용에서 클래스 충돌을 방지하기 위함

--- concurrency-playground/src/section6/T20_LinkedListSample.java
+++ concurrency-playground/src/section6/T20_LinkedListSample.java
@@ -16,68 +16,69 @@ public class T20_LinkedListSample {
 		db.removeAtHead();
 		db.printAll();
 	}
-}
 
-class UserData {
-	UserData(String name) {
-		this.name = name;
-	}
-	String name;
-	UserData prev;
-	UserData next;
-}
-
-class MyList {
-	protected int counter = 0;
-	protected UserData head = new UserData("DummyHead");
-	protected UserData tail = new UserData("DummyTail");
-	MyList() {
-		head.next = tail;
-		tail.prev = head;
-	}
-
-	public int size() {
-		return counter;
-	}
-
-	public boolean appendNode(String name) {
-		UserData newUser = new UserData(name);
-		newUser.prev = tail.prev;
-		newUser.next = tail;
-		tail.prev.next = newUser;
-		tail.prev = newUser;
-
-		++counter;
-		return true;
-	}
-
-	public boolean isEmpty() {
-		if(head.next == tail)
-			return true;
-
-		return false;
-	}
-
-	public void printAll() {
-		System.out.println("-----------------------");
-		System.out.println("Counter: " + counter);
-		UserData tmp = head.next;
-		while(tmp != tail) {
-			System.out.println(tmp.name);
-			tmp = tmp.next;
+	private static class UserData {
+		UserData(String name) {
+			this.name = name;
 		}
-		System.out.println("-----------------------");
+		String name;
+		UserData prev;
+		UserData next;
 	}
 
-	public UserData removeAtHead() {
-		if(isEmpty())
-			return null;
+	private static class MyList {
+		protected int counter = 0;
+		protected UserData head = new UserData("DummyHead");
+		protected UserData tail = new UserData("DummyTail");
 
-		UserData node = head.next;
-		node.next.prev = head;
-		head.next = node.next;
+		MyList() {
+			head.next = tail;
+			tail.prev = head;
+		}
 
-		--counter;
-		return node;
+		public int size() {
+			return counter;
+		}
+
+		public boolean appendNode(String name) {
+			UserData newUser = new UserData(name);
+			newUser.prev = tail.prev;
+			newUser.next = tail;
+			tail.prev.next = newUser;
+			tail.prev = newUser;
+
+			++counter;
+			return true;
+		}
+
+		public boolean isEmpty() {
+			if (head.next == tail)
+				return true;
+
+			return false;
+		}
+
+		public void printAll() {
+			System.out.println("-----------------------");
+			System.out.println("Counter: " + counter);
+			UserData tmp = head.next;
+			while (tmp != tail) {
+				System.out.println(tmp.name);
+				tmp = tmp.next;
+			}
+			System.out.println("-----------------------");
+		}
+
+		public UserData removeAtHead() {
+			if (isEmpty())
+				return null;
+
+			UserData node = head.next;
+			node.next.prev = head;
+			head.next = node.next;
+
+			--counter;
+			return node;
+		}
 	}
 }
